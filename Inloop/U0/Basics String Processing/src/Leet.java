c312d9ed92e240f61a2faed0bc166e8a7df24e92
TheT87 <ytacc87@gmail.com>
2019-04-10 17:44:11 +0200

    leetspeek working

--- Inloop/U0/Basics String Processing/src/Leet.java
+++ Inloop/U0/Basics String Processing/src/Leet.java
@@ -9,31 +9,30 @@ public class Leet {
     }
 
     public static String toLeet(String normal){
-        normal = normal.replaceAll("elite","1337");
-        normal = normal.replaceAll("cool","k3wl");
-        normal = normal.replaceAll("!","!!!11");
-        normal = normal.replaceAll("ck","xx");
-        normal = normal.replaceAll("ers","0rz");
-        normal = normal.replaceAll("er","0rz");
-        normal = normal.replaceAll("en","n");
-        normal = normal.replaceAll("e","3");
-        normal = normal.replaceAll("t","7");
-        normal = normal.replaceAll("o","0");
-        normal = normal.replaceAll("a","@");
-        //System.out.println(normal);
+        normal = normal.replace("elite","1337");
+        normal = normal.replace("cool","k3wl");
+        normal = normal.replace("!","!!!11");
+        normal = normal.replace("ck","xx");
+        normal = normal.replace("ers","0rz");
+        normal = normal.replace("er","0rz");
+        normal = normal.replace("en","n");
+        normal = normal.replace("e","3");
+        normal = normal.replace("t","7");
+        normal = normal.replace("o","0");
+        normal = normal.replace("a","@");
+        System.out.println(normal);
         return normal;
     }
 
 
 
     public static String[] allToLeet(String[] normals){
-        int i;
-        String[] leet = new String[2];
-        for (i = 0;i<leet.length;i++){
+        String[] leet = new String[normals.length];
+        for (int i = 0;i<leet.length;i++){
 
-            String test = Leet.toLeet(normals[i]);
+            String test = toLeet(normals[i]);
             leet[i]=test;
-            System.out.println(leet);
+
         }
 
         return leet;
--- Inloop/U0/Basics String Processing/src/Main.java
+++ Inloop/U0/Basics String Processing/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args){
+
+
         String s1= ("We are elite hackers");
         String s2= ("Informatiker machen coole Sachen!");
+
         Leet.toLeet(s1);
         Leet.toLeet(s2);
 
@@ -9,6 +12,7 @@ public class Main {
         myArray[0] = ("We are elite hackers");
         myArray[1] = ("Informatiker machen coole Sachen!");
         Leet.allToLeet(myArray);
+
 
     }
 }
